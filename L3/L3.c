74ea5154d74e3431f3923362c653689db7efc474
cookin99 <alexd7johnson@gmail.com>
2026-04-02 21:53:34 -0400

    Mileston completed, memory scanner pared with Lab1 to get address

--- L3/L3.c
+++ L3/L3.c
@@ -4,12 +4,17 @@
 #include <Tlhelp32.h>
 
 int main(){
+    //Creates snapshot of all processes open
     HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
+    //Type process, used to step through snapshot
     PROCESSENTRY32 p;
+    //Setting the size of P so that it isn't broken by microsoft updated
     p.dwSize = sizeof(PROCESSENTRY32);
+    //check and step first process in snapshot
     if(Process32First(h,&p)){
         printf("PID: %lu     Name:%s\n ",p.th32ProcessID,p.szExeFile);
     }
+    //step all other processes in snapshot
     while(Process32Next(h,&p)){
         printf("PID: %lu     Name:%s\n ",p.th32ProcessID,p.szExeFile);
     }
